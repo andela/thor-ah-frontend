4d3a768329a5cbbce36d33364223f58439be2afe
lauragift21 <laurandidi21@gmail.com>
2018-10-03 12:48:16 +0100

    chore(tests): Setup test integration with travis
    - Write test for App component
    - Add travis.yml file
    - Add Readme Badges
    
    [Finishes #159987620]

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { create } from 'react-test-renderer';
 import App from "../App";
 
 it("renders without crashing", () => {
@@ -7,3 +8,9 @@ it("renders without crashing", () => {
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
+
+it("create a snapshot", () => {
+  const c = create(<App />);
+  expect(c.toJSON()).toMatchSnapshot();
+});
+
