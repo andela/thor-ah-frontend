e3fdf2a77992a01ddc016c87b83f3c1573698c41
Emmanuel Adesile <emma2adesile@gmail.com>
2018-10-10 10:10:10 +0100

    ft(create-article): add support for uploading images
    [Deliver 159987629]

--- src/containers/CreateArticle/CreateArticlePage.js
+++ src/containers/CreateArticle/CreateArticlePage.js
@@ -1,28 +1,123 @@
 /* eslint-disable */
 import React, { Component, Fragment } from "react";
+import axios from "axios";
 import ArticleTitle from "./ArticleTitle";
 import TextEditor from "./TextEditor";
-import styles from './CreateArticle.module.scss';
+import styles from "./CreateArticle.module.scss";
 
 class CreateArticle extends Component {
-  state = {
-    title: "Title",
-    imageUrl: "",
-    articleBody: "Share your thoughts..."
-  };
+  constructor(props) {
+    super(props);
 
-  titleChangeHandler = (title) => {
+    this.state = {
+      title: "Title",
+      body: "Share your thoughts...",
+      description: "",
+      loading: false
+    };
+  }
+
+  titleChangeHandler = title =>
     this.setState({
       title
-    })
-  }
+    });
+
+  editorChangeHandler = body =>
+    this.setState({
+      body: body
+    });
+
+  titlePlaceholderFocusInHandler = () => {
+    const { title } = this.state;
+    if (title.trim() === "Title") {
+      this.setState({ title: "" });
+    }
+  };
+
+  titlePlaceholderFocusOutHandler = () => {
+    const { title } = this.state;
+    if (title.trim() === "") {
+      this.setState({
+        title: "Title"
+      });
+    }
+  };
+
+  bodyPlaceholderFocusInHandler = () => {
+    const { body } = this.state;
+    if (
+      body.trim() === "Share your thoughts..." ||
+      body.trim() === "<p>Share your thoughts...</p>"
+    ) {
+      this.setState({
+        body: ""
+      });
+    }
+  };
+
+  bodyPlaceholderFocusOutHandler = () => {
+    const { body } = this.state;
+    if (body.trim() === "") {
+      this.setState({
+        body: "Share your thoughts..."
+      });
+    }
+  };
+
+  // titlePasteHandler = (e) => {
+  //   e.preventDefault();
+  //   const content = navigator.clipboard.readText();
+  //   if (typeof clipContent !== 'string') return;
+  //   this.setState({ title: content });
+  //   console.log
+  // }
+
+  imageUploadHandler = (blobInfo, success, failure) => {
+    try {
+      const { imgUrl } = this.state;
+      const formData = new FormData();
+      formData.append(file, blobInfo.blob(), blobInfo.filename());
+      formData.append(upload_preset, "asgjqcgx");
+      formData.append(api_key, "329369412183662");
+      const uploadUrl =
+        "https://api.cloudinary.com/v1_1/dn6fnuhxr/image/upload";
+      return axios
+        .post(uploadUrl, formData, {
+          headers: {
+            "Content-Type": "Application/x-www-form-urlencoded",
+            "X-Requested-With": "XMLHttpRequest"
+          }
+        })
+        .then(response => {
+          const data = response.data;
+          const fileUrl = data.secure_url;
+          console.log(data);
+          success(result.data.secure_url);
+        });
+    } catch (error) {
+      failure("file upload failed", console.log(error));
+    }
+  };
 
   handleSubmit = () => {
-    
-  }
+    const { title, body } = this.state;
+
+    console.log(this.rootNode.props.value);
+
+    axios
+      .post("https://thor-ah-staging.herokuapp.com/api/articles", {
+        title,
+        body
+      })
+      .then(response => response.json())
+      .then(data => {
+        console.log(data);
+      })
+      .catch(error => console.log(error));
+  };
 
   render() {
-    const { title, imageUrl, articleBody } = this.state;
+    const { title, imageUrl, body } = this.state;
     return (
       <Fragment>
         <div className="col-md-8 mx-auto my-5">
@@ -30,22 +125,39 @@ class CreateArticle extends Component {
             <div className={styles.articleTitle}>
               <ArticleTitle
                 value={title}
-                onChange={this.titleChangeHandler}
-                onFocusIn={this.placeholderFocusInHandler}
-                onEditorChange={this.placeholderFocusOutHandler}
-                onPaste={this.pasteHandler}
+                titleChangeHandler={this.titleChangeHandler}
+                titlePlaceholderFocusInHandler={
+                  this.titlePlaceholderFocusInHandler
+                }
+                titlePlaceholderFocusOutHandler={
+                  this.titlePlaceholderFocusOutHandler
+                }
+                titlePasteHandler={this.titlePasteHandler}
               />
             </div>
             <div className="textEditor">
               <TextEditor
-                value={articleBody}
-                onEditorChange={this.editorChangeHandler}
-                onFocusIn={this.placeholderFocusInHandler}
-                onFocusOut={this.placeholderFocusOutHandler}
+                ref={node => (this.rootNode = node)}
+                value={body}
+                editorChangeHandler={this.editorChangeHandler}
+                bodyPlaceholderFocusInHandler={
+                  this.bodyPlaceholderFocusInHandler
+                }
+                bodyPlaceholderFocusOutHandler={
+                  this.bodyPlaceholderFocusOutHandler
+                }
+                imageUploadHandler={this.imageUploadHandler}
               />
             </div>
           </div>
-          <button className={styles.publishButton} onClick={this.handleSubmit}>Publish</button>
+          <div className="mt-3 buttonDiv">
+            <button
+              className={styles.publishButton}
+              onClick={this.handleSubmit}
+            >
+              Publish
+            </button>
+          </div>
         </div>
       </Fragment>
     );
--- src/containers/CreateArticle/TextEditor.js
+++ src/containers/CreateArticle/TextEditor.js
@@ -1,55 +1,61 @@
-import React from "react";
+import React, { Component } from "react";
 import { Editor } from "@tinymce/tinymce-react";
-import PropTypes from 'prop-types';
+import PropTypes from "prop-types";
 
 const apiKey = "0naso5vy7fw5eu2eqblor00o1c326x0hupermg4cli0rqoqn";
 
-const TextEditor = ({
-  imageUploadHandler,
-  editorChangeHandler,
-  value,
-  placeholderFocusInHandler,
-  placeholderFocusOutHandler
-}) => (
-    <Editor
-      apiKey={apiKey}
-      value={value}
-      onChange={editorChangeHandler}
-      onFocusIn={placeholderFocusInHandler}
-      onFocusOut={placeholderFocusOutHandler}
-      init={{
-        menubar: false,
-        theme: "inlite",
-        inline: true,
-        mobile: {
-          theme: "mobile",
-          plugins: ["autosave", "lists", "autolink"]
-        },
-        plugins: [
-          "autolink",
-          "codesample",
-          "link",
-          "linkchecker",
-          "lists",
-          "mediaembed",
-          "textcolor",
-          "image",
-          "emoticons"
-        ],
-        selection_toolbar:
-          "bold italic underline strikethrough| h2 h3 | blockquote quicklink | alignleft aligncenter alignright alignfull",
-        images_upload_handler: imageUploadHandler,
-        images_reuse_filename: true,
-        toolbar: "emoticons"
-      }}
-    />
-);
+/* eslint-disable-next-line */
+class TextEditor extends Component {
+  render() {
+    const {
+      value,
+      imageUploadHandler,
+      editorChangeHandler,
+      bodyPlaceholderFocusInHandler,
+      bodyPlaceholderFocusOutHandler
+    } = this.props;
+    return (
+      <Editor
+        apiKey={apiKey}
+        value={value}
+        onEditorChange={editorChangeHandler}
+        onFocusIn={bodyPlaceholderFocusInHandler}
+        onFocusOut={bodyPlaceholderFocusOutHandler}
+        init={{
+          menubar: false,
+          theme: "inlite",
+          inline: true,
+          mobile: {
+            theme: "mobile",
+            plugins: ["autosave", "lists", "autolink"]
+          },
+          plugins: [
+            "autolink",
+            "codesample",
+            "link",
+            "linkchecker",
+            "lists",
+            "mediaembed",
+            "textcolor",
+            "image",
+            "emoticons"
+          ],
+          selection_toolbar:
+            "bold italic underline strikethrough| h2 h3 | blockquote quicklink | alignleft aligncenter alignright alignfull",
+          images_upload_handler: imageUploadHandler,
+          images_reuse_filename: true,
+          toolbar: "emoticons"
+        }}
+      />
+    );
+  }
+}
 
 TextEditor.propTypes = {
   imageUploadHandler: PropTypes.func.isRequired,
   editorChangeHandler: PropTypes.func.isRequired,
-  placeholderFocusInHandler: PropTypes.func.isRequired,
-  placeholderFocusOutHandler: PropTypes.func.isRequired
-}
+  bodyPlaceholderFocusInHandler: PropTypes.func.isRequired,
+  bodyPlaceholderFocusOutHandler: PropTypes.func.isRequired
+};
 
 export default TextEditor;
