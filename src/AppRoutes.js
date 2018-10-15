f233df5d07464da8cd9e548bbe429e2ebb5b4512
Emmanuel Adesile <emma2adesile@gmail.com>
2018-10-15 12:35:36 +0100

    fix user create article feature

--- src/AppRoutes.js
+++ src/AppRoutes.js
@@ -1,44 +1,39 @@
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import React, { Fragment } from "react";
 // components
-import ProfilePage from './containers/ProfilePage';
-import ArticlePage from './containers/ArticlePage';
-import Homepage from './containers/Home/Home';
-import Header from './components/Header/Header';
-import Footer from './components/Footer/Footer';
-import ProtectedRoute from './containers/ProtectedRoute';
-import Signin from './containers/SignIn/SignIn';
-import AllCategories from './containers/Categories/Categories';
-import ArticleCategory from './containers/Categories/SingleCategory';
-import NavBar from './components/NavBar';
-import CreateArticlePage from './containers/CreateArticle/CreateArticlePage';
+import ProfilePage from "./containers/ProfilePage";
+import ArticlePage from "./containers/ArticlePage";
+import Homepage from "./containers/Home/Home";
+import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
+import ProtectedRoute from "./containers/ProtectedRoute";
+import Signin from "./containers/SignIn/SignIn";
+import AllCategories from "./containers/Categories/Categories";
+import ArticleCategory from "./containers/Categories/SingleCategory";
+import CreateArticlePage from "./containers/CreateArticle/CreateArticlePage";
 import ErrorPage from "./containers/Error";
 
 const AppRoutes = () => (
   <Router>
     <Fragment>
       <Header />
-      <Route exact path='/' component={Homepage} />
-      <Route path='/categories' component={ AllCategories }/>
-      <Route path='/category/:name' component={ ArticleCategory } />
-      <Route exact path='/signin' component={ Signin }/>
-      <Route exact path='/' component={ Homepage }/>
-      <Route exact path='/create-article' component={ CreateArticlePage }/>
       <Route exact path="/" component={Homepage} />
+      <Route path="/categories" component={AllCategories} />
+      <Route path="/category/:name" component={ArticleCategory} />
       <Route exact path="/signin" component={Signin} />
       <Route exact path="/error" component={ErrorPage} />
       {/* protected routes can be rendered using the ProtectedRoutes component */}
-      <NavBar />
-      <ProtectedRoute path='/articles/:slug' component={ ArticlePage }/>
-      <ProtectedRoute path='/profile/user' component={ ProfilePage }/>
-      <ProtectedRoute path='/article' component={ArticlePage} />
-      <ProtectedRoute path='/profile/user' component={ProfilePage} />
-      <Route exact path='/signin' component={Signin} />
-      <ProtectedRoute path='/article' component={ ArticlePage }/>
-      <ProtectedRoute path='/profile/user' component={ ProfilePage }/>
+      <ProtectedRoute path="/articles/:slug" component={ArticlePage} />
+      <ProtectedRoute path="/profile/user" component={ProfilePage} />
       <ProtectedRoute path="/article" component={ArticlePage} />
       <ProtectedRoute path="/profile/user" component={ProfilePage} />
-      <ProtectedRoute exact path="/create-article" component={CreateArticlePage} />
+      <ProtectedRoute path="/article" component={ArticlePage} />
+      <ProtectedRoute path="/profile/user" component={ProfilePage} />
+      <ProtectedRoute
+        exact
+        path="/create-article"
+        component={CreateArticlePage}
+      />
       <Footer />
     </Fragment>
   </Router>
