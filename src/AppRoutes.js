1f0531d699b4ba7099d6b0f0da9e4f1b43e6c3d5
Uche Jude <iamuchejude@gmail.com>
2018-10-16 06:08:52 +0100

    feat(userProfile): Update user profile

--- src/AppRoutes.js
+++ src/AppRoutes.js
@@ -1,6 +1,5 @@
-
-import { BrowserRouter as Router, Route } from "react-router-dom";
 import React, { Fragment } from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 // components
 import ProfilePage from "./containers/ProfilePage";
 import ArticlePage from "./containers/ArticlePage";
@@ -13,19 +12,6 @@ import AllCategories from "./containers/Categories/Categories";
 import ArticleCategory from "./containers/Categories/SingleCategory";
 import CreateArticlePage from "./containers/CreateArticle/CreateArticlePage";
 import ErrorPage from "./containers/Error";
-
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import React, { Fragment } from 'react'
-// components
-import ProfilePage from './containers/ProfilePage';
-import ArticlePage from './containers/ArticlePage';
-import Homepage from './containers/Home/Home';
-import Header from './components/Header/Header';
-import Footer from './components/Footer/Footer';
-import ProtectedRoute from './containers/ProtectedRoute';
-import Signin from './containers/SignIn/SignIn';
-import AllCategories from './containers/Categories/Categories';
-import ArticleCategory from './containers/Categories/SingleCategory';
 import NotFound from './containers/NotFound/NotFound';
 
 const AppRoutes = () => (
@@ -36,29 +22,21 @@ const AppRoutes = () => (
       <Route path="/categories" component={AllCategories} />
       <Route path="/category/:name" component={ArticleCategory} />
       <Route exact path="/signin" component={Signin} />
-      <Route exact path="/error" component={ErrorPage} />
-      {/* protected routes can be rendered using the ProtectedRoutes component */}
-      <ProtectedRoute path="/articles/:slug" component={ArticlePage} />
-      <ProtectedRoute path="/profile/user" component={ProfilePage} />
-      <ProtectedRoute path="/article" component={ArticlePage} />
-      <ProtectedRoute path="/profile/user" component={ProfilePage} />
-      <ProtectedRoute path="/article" component={ArticlePage} />
-      <ProtectedRoute path="/profile/user" component={ProfilePage} />
-      <ProtectedRoute
-        exact
-        path="/create-article"
-        component={CreateArticlePage}
-      />
-
-      <ProtectedRoute path='/profile/user' component={ProfilePage} />
       <Switch>
         <Route exact path='/' component={Homepage} />
         {/* protected routes can be rendered using the ProtectedRoutes component */}
         <Route path='/categories' component={AllCategories} />
         <Route path='/category/:name' component={ArticleCategory} />
-        <ProtectedRoute path='/articles/:slug' component={ArticlePage} />
         <ProtectedRoute path='/profile/user' component={ProfilePage} />
-        <Route exact path='/signin' component={Signin} />
+        <Route exact path="/error" component={ErrorPage} />
+        {/* protected routes can be rendered using the ProtectedRoutes component */}
+        <ProtectedRoute path="/articles/:slug" component={ArticlePage} />
+        <ProtectedRoute path="/article" component={ArticlePage} />
+        <ProtectedRoute
+          exact
+          path="/create-article"
+          component={CreateArticlePage}
+        />
 
         {/* will always render when no other path is matched */}
         <Route component={NotFound} />
