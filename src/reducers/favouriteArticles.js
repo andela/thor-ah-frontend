1dafeef0bb2d3e128137346b0873728507025657
Uche Jude <iamuchejude@gmail.com>
2018-10-23 16:28:38 +0100

    nitpick: fix typo error

--- src/reducers/favouriteArticles.js
+++ src/reducers/favouriteArticles.js
@@ -1,11 +1,11 @@
-import initalState from '../store/initialState';
+import initialState from '../store/initialState';
 import {
   FETCH_FAVOURITE_ARTICLES_REQUEST,
   FETCH_FAVOURITE_ARTICLES_SUCCESS,
   FETCH_FAVOURITE_ARTICLES_FAILED,
 } from '../actionTypes/favouriteArticles';
 
-const favouriteArticles = (state = initalState.favouriteArticles, action) => {
+const favouriteArticles = (state = initialState.favouriteArticles, action) => {
   switch (action.type) {
     case FETCH_FAVOURITE_ARTICLES_SUCCESS:
       return {
