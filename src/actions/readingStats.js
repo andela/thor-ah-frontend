1bfe8a4f582a65f9aa036b63c09b19deac98934c
Uche Jude <iamuchejude@gmail.com>
2018-10-23 11:37:27 +0100

    nitpick: corrected naming conventions

--- src/actions/readingStats.js
+++ src/actions/readingStats.js
@@ -6,9 +6,9 @@ export const readingStatsRequesting = state => ({
   payload: state,
 })
 
-export const readingStatsError = err => ({
+export const readingStatsError = error => ({
   type: FETCH_READING_STATS_FAILED,
-  payload: err,
+  payload: error,
 });
 
 export const showReadingStats = data => ({
@@ -20,32 +20,34 @@ const API = process.env.REACT_APP_API;
 
 export const getReadingStats = () => dispatch => {
   dispatch(readingStatsRequesting(true));
-  const { token } = window.localStorage;
-    return axios
-      .get(`${API}/api/user-reading-stats`, {
-        headers: {
-          'Authorization': `Bearer ${token}`,
-          'Content-type': 'application/json',
-        }
-      })
-      .then(response => {
-        const { data } = response;
-        if (data.status === 'error') {
-          dispatch(readingStatsError(data.error.message));
-          return dispatch(readingStatsRequesting(false));
-        }
-        const { numberOfArticlesRead, mostReadCategory, articleReactions } = data;
-        const payload = {
-          mostReadCategory,
-          numberOfArticlesRead,
-          articleReactions,
-        }
-        dispatch(showReadingStats(payload))
+
+  const { token } = localStorage;
+
+  return axios
+    .get(`${API}/api/user-reading-stats`, {
+      headers: {
+        'Authorization': `Bearer ${token}`,
+        'Content-type': 'application/json',
+      }
+    })
+    .then(response => {
+      const { data } = response;
+      if (data.status === 'error') {
+        dispatch(readingStatsError(data.error.message));
         return dispatch(readingStatsRequesting(false));
-      })
-      .catch(() => {
-        const errorMessage = "Error occurred.Please try again later";
-        dispatch(readingStatsError(errorMessage));
-        return dispatch(readingStatsRequesting(false));
-      })
+      }
+      const { numberOfArticlesRead, mostReadCategory, articleReactions } = data;
+      const payload = {
+        mostReadCategory,
+        numberOfArticlesRead,
+        articleReactions,
+      }
+      dispatch(showReadingStats(payload))
+      return dispatch(readingStatsRequesting(false));
+    })
+    .catch(() => {
+      const errorMessage = "Error occurred.Please try again later";
+      dispatch(readingStatsError(errorMessage));
+      return dispatch(readingStatsRequesting(false));
+    })
 };
